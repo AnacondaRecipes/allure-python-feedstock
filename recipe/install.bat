88277c7828a57443d4b85aeb56ba4a209227c978
Andrii-Osipov <aosipov@anaconda.com>
2025-05-02 03:01:35 -0700

    PKG-7909: allure-python 2.14.0
    PKG-7909: allure-python 2.14.0

--- recipe/install.bat
+++ recipe/install.bat
@@ -1,2 +1,9 @@
 SET SETUPTOOLS_SCM_PRETEND_VERSION=%PKG_VERSION%
+
+:: UnicodeDecodeError: 'charmap' codec can't decode byte 0x9d in position 808: character maps to <undefined>
+REM set UTF-8 mode by default
+chcp 65001
+set PYTHONUTF8=1
+set PYTHONIOENCODING="UTF-8"
+
 %PYTHON% -m pip install --no-deps  --no-build-isolation --ignore-installed -vv ./%PKG_NAME%/
